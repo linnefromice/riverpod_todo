f6716990bd788ae3dc78a566bd0dbc9361707c8e
linnefromice <linnefromice@gmail.com>
2021-05-28 00:10:31 +0900

    create model of todo

---
+++ lib/models/todo.dart
@@ -0,0 +1,11 @@
+class Todo {
+  Todo({
+    required this.id,
+    required this.description,
+    this.completed = false
+  });
+
+  final String id;
+  final String description;
+  final bool completed;
+}
