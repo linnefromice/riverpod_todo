dda2f8daadb27f65423aa5435e10c8b66807cbfd
linnefromice <linnefromice@gmail.com>
2021-06-04 23:52:42 +0900

    update todo_item_test

--- test/screens/components/todo_item_test.dart
+++ test/screens/components/todo_item_test.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:riverpod_todo/models/todo.dart';
 import 'package:riverpod_todo/screens/components/todo_item.dart';
-import 'package:riverpod_todo/screens/components/toolbar.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_todo/screens/home_page.dart';
 
 void main() {
-  testWidgets('Title has a title', (tester) async {
-    await tester.pumpWidget(const MaterialApp(
-      home: Scaffold(
-        body: TodoItem()
+  testWidgets('default display', (tester) async {
+    await tester.pumpWidget(ProviderScope(
+      overrides: [
+        currentTodo.overrideWithValue(Todo(
+          id: 'test_id',
+          description: 'test_description',
+          completed: true
+        ))
+      ],
+      child: MaterialApp(
+        home: TodoItem()
       ),
     ));
-    expect(find.text('todo.description'), findsOneWidget);
+    expect(find.byType(ListTile), findsOneWidget);
+    expect(find.byType(Checkbox), findsOneWidget);
+    expect(find.byType(Text), findsOneWidget);
   });
 }
