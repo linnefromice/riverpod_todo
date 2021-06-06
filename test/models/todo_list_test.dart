5d357c85fc3f19bfc0e710a7585126651650ad5e
linnefromice <linnefromice@gmail.com>
2021-06-06 22:53:30 +0900

    update todo_list_test

--- test/models/todo_list_test.dart
+++ test/models/todo_list_test.dart
@@ -3,6 +3,12 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:riverpod_todo/models/todo.dart';
 import 'package:riverpod_todo/models/todo_list.dart';
 
+StateNotifierProvider<TodoList, List<Todo>> createNotifier(List<Todo> list) {
+  return StateNotifierProvider<TodoList, List<Todo>>(
+    (ref) => TodoList(list)
+  );
+}
+
 void main() {
   var uncompleted = Todo(
     id: 'id_uncompleted',
@@ -13,15 +19,12 @@ void main() {
     description: 'description_completed',
     completed: true
   );
-  final todoListProvider = StateNotifierProvider<TodoList, List<Todo>>(
-    (ref) => TodoList([
-      uncompleted,
-      completed
-    ]
-  ));
   test('read provider', () {
     final container = ProviderContainer();
-    final list = container.read(todoListProvider);
+    final list = container.read(createNotifier([
+      uncompleted,
+      completed
+    ]));
     expect(list.length, 2);
     expect(list[0].id, uncompleted.id);
     expect(list[0].description, uncompleted.description);
@@ -33,26 +36,61 @@ void main() {
 
   test('TodoList#add', () {
     final container = ProviderContainer();
-    final notifier = container.read(todoListProvider.notifier);
+    final provider = createNotifier([
+      uncompleted,
+      completed
+    ]);
+    final notifier = container.read(provider.notifier);
     var description = "NEW DESCRIPTION";
     notifier.add(description);
-    final list = container.read(todoListProvider);
+    final list = container.read(provider);
     expect(list.length, 3);
     expect(list[2].description, description);
     expect(list[2].completed, false);
   });
 
+  test('TodoList#toggle', () {
+    final container = ProviderContainer();
+    final provider = createNotifier([uncompleted]);
+
+    final beforeList = container.read(provider);
+    expect(beforeList[0].completed, false);
+
+    container.read(provider.notifier).toggle(uncompleted.id);
+
+    final afterList = container.read(provider);
+    expect(afterList[0].completed, true);
+  });
+
+  test('TodoList#edit', () {
+    final container = ProviderContainer();
+    final provider = createNotifier([uncompleted]);
+
+    final editDescription = "EDIT Description";
+    container.read(provider.notifier).edit(
+      id: uncompleted.id,
+      description: editDescription
+    );
+
+    final list = container.read(provider);
+    expect(list[0].description, editDescription);
+  });
+
   test('TodoList#remove', () {
     final container = ProviderContainer();
-    final notifier = container.read(todoListProvider.notifier);
+    final provider = createNotifier([
+      uncompleted,
+      completed
+    ]);
+    final notifier = container.read(provider.notifier);
 
     notifier.remove(uncompleted);
-    final firstList = container.read(todoListProvider);
+    final firstList = container.read(provider);
     expect(firstList.length, 1);
     expect(firstList[0].description, completed.description);
 
     notifier.remove(completed);
-    final secondList = container.read(todoListProvider);
+    final secondList = container.read(provider);
     expect(secondList.length, 0);
   });
 }
