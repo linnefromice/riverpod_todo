8a7ddae5900a5e7bd962a8d76e4d8b70190770a0
linnefromice <linnefromice@gmail.com>
2021-06-06 21:57:51 +0900

    update todo test

--- test/models/todo_test.dart
+++ test/models/todo_test.dart
@@ -4,9 +4,17 @@ import 'package:riverpod_todo/models/todo.dart';
 import 'package:riverpod_todo/screens/components/title.dart' as c;
 
 void main() {
-  test('todo model has attributes', () {
+  test('required attributes', () {
     final todo = Todo(id: "1", description: "description");
     expect(todo.id, "1");
     expect(todo.description, "description");
+  });
+  test('when default completed parameter', () {
+    final todo = Todo(id: "1", description: "description");
+    expect(todo.completed, false);
+  });
+  test('when completed parameter is assigned "true"', () {
+    final todo = Todo(id: "1", description: "description", completed: true);
+    expect(todo.completed, true);
   });
 }
